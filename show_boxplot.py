257212890b4d756641e5c4f9b5367945062d73ef
hokusei <hokuseihal@gmail.com>
2020-05-20 07:49:13 +0900

    fix

--- show_boxplot.py
+++ show_boxplot.py
@@ -4,4 +4,4 @@ import sys
 with open(f'{sys.argv[1]}/data.pkl','rb') as f:
     data=pickle.load(f)
 plt.boxplot(data['acc:miou'])
-plt.savefig('boxplot.png')
+plt.savefig(f'{sys.argv[1]}/boxplot.png')
