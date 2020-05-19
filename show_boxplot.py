feef0a6c335622237985b9f1d65e69f73012fc74
hokusei <hokuseihal@gmail.com>
2020-05-20 08:00:45 +0900

    fix

--- show_boxplot.py
+++ show_boxplot.py
@@ -1,6 +1,9 @@
 import pickle
 import matplotlib.pyplot as plt
 import sys
+folder=sys.argv[1]
+if folder[-1]!='/':
+    folder=folder+'/'
 with open(f'{sys.argv[1]}/data.pkl','rb') as f:
     data=pickle.load(f)
 plt.boxplot(data['acc:miou'])
